611ce5256d261b5bc3bf287fb6e40590ff57c9b2
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-14 14:39:45 +0200

    start

---
+++ script.js
@@ -0,0 +1,17 @@
+function perform() {
+  //TODO implement
+}
+
+perform(null, function(value) { // value === null
+  var param = 1;
+  console.log(param); // 1
+  return param;
+})
+.then(function(param) { // param === 1
+  console.log(++param); // 2
+  return param;
+})
+.then(function(param) { // param === 2
+  console.log(++param); // 3
+  return param;
+});
