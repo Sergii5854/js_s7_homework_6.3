6732cd0e80740eef4ff63516ff1df1ce7485158f
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-15 22:12:54 +0200

    done

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 function perform() {
-var argumentsToArray =  function(args) { 
-    if(args !=='object') {
-    return [].slice.apply(args); 
-   }else{
-     return [].slice.apply(args); 
-   }
+
+  var argumentsToArray = function (args) {
+    return [].slice.apply(args);
   }
+
   var callback;
   var args = argumentsToArray(arguments);
-  args.forEach(function(value) {  
-    argumentsToArray = value
-   });
+  args.forEach(function (value) {
+    callback = value
+  });
 
-  var res = argumentsToArray(null, args)
-    this.then = function (argumentsToArray) {
-        return perform(res, argumentsToArray)
-    }
-    return this
+  var res = callback.apply(null, args)
+  this.then = function (callback) {
+    return perform(res, callback)
+  }
+  return this
 }
 
-perform(null, function(value) { // value === null
+perform(null, undefined, 0, 'string', null, function (value) { // value === null
   var param = 1;
   console.log(param); // 1
   return param;
 })
-.then(function(param) { // param === 1
-  console.log(++param); // 2
-  return param;
-})
-.then(function(param) { // param === 2
-  console.log(++param); // 3
-  return param;
-});
+    .then(function (param) { // param === 1
+      console.log(++param); // 2
+      return param;
+    })
+    .then(function (param) { // param === 2
+      console.log(++param); // 3
+      return param;
+    });
 
 //https://stackoverflow.com/questions/4633125/is-it-possible-to-get-all-arguments-of-a-function-as-single-object-inside-that-f
